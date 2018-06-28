92303161c8d649089ea16242098e56fe13cc4135
Aditya Pradhan <adityapradhan42@gmail.com>
2018-06-28 10:45:37 +0530

    Implemented Back-end Server

---
+++ server.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
+    ]
+}
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    // Load hash from your password DB.
+    bcrypt.compare("bacon", hash, function(err, res) {
+        // res == true
+    });
+    bcrypt.compare("veggies", hash, function(err, res) {
+     // res = false
+    });
+    
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password)
+    {
+        res.json(database.users[0]);
+    }
+    else
+    {
+        res.status(400).json("Error logging in!");
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    
+    bcrypt.hash(password, null, null, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash);
+    });
+
+    database.users.push({
+            id: '125',
+            name: name,
+            email: email,
+            entries: 0,
+            joined: new Date()
+    }
+)
+
+    res.json(database.users[database.users.length - 1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+
+    if (!found) 
+    {
+        res.status(404).json("Not found!");
+    }
+});
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+
+    if (!found) 
+    {
+        res.status(404).json("Not found!");
+    }
+})
+
+app.listen(3000, () => {
+    console.log('App is running on port 3000');
+});
+
+
+/*
+/ --> res = this is working
+/signin --> POST (because we are posting some user information) = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> updated user object
+*/
