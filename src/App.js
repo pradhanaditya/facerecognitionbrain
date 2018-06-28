188007419d6513f123d611ed25cb9463ca342a12
Aditya Pradhan <adityapradhan42@gmail.com>
2018-06-28 10:47:47 +0530

    Implemented Back-end server

--- src/App.js
+++ src/App.js
@@ -36,10 +36,27 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
-  
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -64,7 +81,26 @@ class App extends Component {
 
   onButtonSubmit = () => {
    this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, {entries: count}))
+          })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+  })
     .catch(err => console.log(err));
   }
 
@@ -89,11 +125,11 @@ class App extends Component {
     { this.state.route === 'home' ?
         <div>
           <Logo /> 
-          <Rank />
+          <Rank name={this.state.user.name} entries={this.state.user.entries}/>
           <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
           <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
         </div> : (
-          this.state.route === 'signin' ? <Signin onRouteChange={this.onRouteChange}/> : <Register onRouteChange={this.onRouteChange}/>
+          this.state.route === 'signin' ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/> : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
         )
         }
         </div>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
     return (
         <div>
             <div className="white f3">
-                {'Andrei, your current rank is...'}
+                {`${name}, your current entry count is...`}
+                <div className='white f1'>
+                    {entries}
+                </div>
             </div>
             <div className="white f3">
                 {'#5'}
